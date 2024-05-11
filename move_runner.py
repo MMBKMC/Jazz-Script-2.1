153ee7a9f0a4d63346ad522062a2570c059bd98f
MMBKMC <151632121+MMBKMC@users.noreply.github.com>
2024-05-11 10:05:02 +0630

    Add files via upload

---
+++ move_runner.py
@@ -0,0 +1,15 @@
+import main
+import time
+from main import token_output1
+
+print("[Hello World]")
+time.sleep(2.1)
+print(" [Hello World]")
+time.sleep(2.1)
+print("  [Hello World]")
+time.sleep(2.1)
+print("   [Hello World]")
+time.sleep(2.1)
+print("    [Hello World]")
+
+open('test1.jzz','r').read()
